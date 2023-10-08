6d286ed503ac35ae144a3816b0894887c4668976
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2023-10-08 19:42:55 +0700

    Update layout.blade.php

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -125,7 +125,7 @@
                         <input name="q" type="search" id="search-dropdown"
                             value="{{ $request != null ? $request['q'] : null }}"
                             class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                            placeholder="Cari Data Mengenai HIV">
+                            placeholder="Cari Data Mengenai {{ $virus->name }}">
                         <button type="submit"
                             class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor"
