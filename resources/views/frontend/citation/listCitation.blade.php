8481e701a369c217bfeea3c54a46a0d775f7ea35
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2023-10-08 19:53:47 +0700

    fix error search citation

--- resources/views/frontend/citation/listCitation.blade.php
+++ resources/views/frontend/citation/listCitation.blade.php
@@ -5,56 +5,75 @@
     }
 </style>
 @section('content')
-    <section class="px-5 py-2 align-top "> 
+    <section class="px-5 py-2 align-top ">
         <p id="tableInfo" class="pt-2"></p>
         <form action="{{ route('downloadFasta') }}" method="get">
-        <div class="dt-responsive table-responsive">
-            <table class="table nowrap" style="border:none" id="samplesTable">
-                <thead style="display: none">
-                    <tr>
-                        <th style="width: 1em">Column 1</th>
-                        <th style="width: 90%; background-color: red">Column 2</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($listCitations as $item)
-                    <tr>
-                        <td style="width: 1;border:none" class="align-top">
-                            <input class="form-check-input" type="checkbox" id="id_fasta" name="fasta[]" value="{{ $item['id_citation'] }}" class="id_fasta">
-                            <p class="text-center">{{ $loop->iteration }}</p>
-                        </td>
-                        <td style="width: 10px;  ">
-                            <div class="text-start">
-                                <div class="mb-1 text-xs text-gray-400">{{ $item['user'] }}</div>
-                                <div class="container-title" >
-                                    <a href="{{ route('detailCitation', $item['id_citation']) }}" class="text-blue-500" style="word-break: break-word;white-space:normal;">{{ $item['title'] }}</a>
-                                </div>
-                                <h6 class="pb-0">{{ $item['province'].",".$item['regency'] }}</h6>
-                                <p>{{ $item['author']->name.",".$item['author']->member }} | {{ $item['monthYear'] }}</p>
-                                <span class="text-gray-400">Accession NCBI : {{ $item['accession_ncbi'] }}</span><span class="text-gray-400"> | Accession INDAGI : {{ $item['accession_indagi'] }}</span>
-                                <h6><a href="{{ route('detailFasta', $item['id_citation']) }}" class="text-blue-500">Fasta</a> </h6>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <button class="download" id="download" type="submit" class="hover:text-blue-800">Download selected</button>
-            </form>
+            <div class="dt-responsive table-responsive">
+                <table class="table nowrap" style="border:none" id="samplesTable">
+                    <thead style="display: none">
+                        <tr>
+                            <th style="width: 1em">Column 1</th>
+                            <th style="width: 90%; background-color: red">Column 2</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($listCitations as $item)
+                            <tr>
+                                <td style="width: 1;border:none" class="align-top">
+                                    <input class="form-check-input" type="checkbox" id="id_fasta" name="fasta[]"
+                                        value="{{ $item['id_citation'] }}" class="id_fasta">
+                                    <p class="text-center">{{ $loop->iteration }}</p>
+                                </td>
+                                <td style="width: 10px;  ">
+                                    <div class="text-start">
+                                        <div class="mb-1 text-xs text-gray-400">{{ $item['user'] }}</div>
+                                        <div class="container-title">
+                                            <a href="{{ route('detailCitation', $item['id_citation']) }}"
+                                                class="text-blue-500"
+                                                style="word-break: break-word;white-space:normal;">{{ $item['title'] }}</a>
+                                        </div>
+                                        <h6 class="pb-0">{{ $item['province'] . ',' . $item['regency'] }}</h6>
+                                        <p>{{ $item['author']['name'] . ',' . $item['author']['member'] }} |
+                                            {{ $item['monthYear'] }}</p>
+                                        <span class="text-gray-400">Accession NCBI :
+                                            {{ $item['accession_ncbi'] }}</span><span class="text-gray-400"> | Accession
+                                            INDAGI : {{ $item['accession_indagi'] }}</span>
+                                        <h6><a href="{{ route('detailFasta', $item['id_citation']) }}"
+                                                class="text-blue-500">Fasta</a> </h6>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <button class="download" id="download" type="submit" class="hover:text-blue-800">Download
+                    selected</button>
+        </form>
         </div>
     </section>
 @endsection
 @push('js-internal')
     <script>
-         $(function () {
+        $(function() {
+            $('#search-dropdown').on('keyup', function() {
+                if ($(this).val() == '') {
+                    $('button[type="submit"]').prop('disabled', true);
+                } else {
+                    $('button[type="submit"]').prop('disabled', false);
+                }
+            });
+        });
+    </script>
+    <script>
+        $(function() {
             var table = $('#samplesTable').DataTable({
                 responsive: true,
-                dom:'rtp'
+                dom: 'rtp'
             });
             var info = table.page.info();
             console.log(info);
             $('#tableInfo').html(
-                'Items : '+(info.page+1)+' to '+info.length+' of ' +info.pages 
+                'Items : ' + (info.page + 1) + ' to ' + info.length + ' of ' + info.pages
             );
         });
         // var checkboxes = document.querySelectorAll("input[type=checkbox]");
@@ -106,6 +125,6 @@
         //     document.body.removeChild(element);
         // }
 
-// Start file download.
+        // Start file download.
     </script>
 @endpush
--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -126,7 +126,7 @@
                             value="{{ $request != null ? $request['q'] : null }}"
                             class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
                             placeholder="Cari Data Mengenai {{ $virus->name }}">
-                        <button type="submit"
+                        <button type="submit" disabled
                             class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor"
                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -186,6 +186,19 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
             integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script>
+            $(function() {
+                $('#search-dropdown').on('keyup', function() {
+                    if ($(this).val() == '') {
+                        $('button[type="submit"]').prop('disabled', true);
+                    } else {
+                        $('button[type="submit"]').prop('disabled', false);
+                    }
+                });
+            });
+        </script>
+
         <script>
             // show loading when website loaded and all assets loaded and ajax request and stop loading when all assets loaded
             $(window).on('load', function() {
