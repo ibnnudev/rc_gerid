b85fb918e1c54098b5ad1192bfabe6ccb63d90c9
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-04-27 16:02:03 +0700

    Remove filter for active users in UserManagementController.php

--- app/Http/Controllers/Admin/UserManagementController.php
+++ app/Http/Controllers/Admin/UserManagementController.php
@@ -24,10 +24,6 @@ class UserManagementController extends Controller
     {
         $user = User::all();
         $user = $user->sortByDesc('created_at');
-        // filter only active user
-        $user = $user->filter(function ($value, $key) {
-            return $value->is_active == 1;
-        });
         if ($request->wantsJson()) {
             return datatables()
                 ->of($user)
