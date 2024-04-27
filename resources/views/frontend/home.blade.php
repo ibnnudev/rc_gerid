ff704ca91a87fd80c6f098516c2ed5d90a92f91d
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-04-28 05:54:20 +0700

    Update content limit in home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -20,7 +20,7 @@
                                             </h1>
                                             <div
                                                 class="space-y-3 text-gray-600 text-sm mt-3 overflow-hidden h-[17em] text-justify leading-6">
-                                                {!! Str::words(html_entity_decode($slide->content), 50, '...') !!}
+                                                {!! Str::limit(html_entity_decode($slide->content), 1200) !!}
                                                 <p class="text-blue-500 font-medium hover:underline">
                                                     <a href="{{ route('slide.show', $slide->slug) }}">Selengkapnya</a>
                                                 </p>
