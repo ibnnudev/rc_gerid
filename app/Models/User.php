9460bad4137b3cdc0f553c29c3cb7e1e1e6b47da
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-04-28 11:40:44 +0700

    Remove HasUuids trait from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, HasUuids, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     const USER_ROLE = 'user';
 
